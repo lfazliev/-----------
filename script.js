cb59a43a27a3e9f7b321be4e5e8e9809eb01b238
listok195 <listok195@gmail.com>
2022-08-17 21:15:55 +0300

    h

--- script.js
+++ script.js
@@ -9,5 +9,8 @@ window.addEventListener('scroll', function () {
 })
 up.style.display = 'none'
 up.onclick = function () {
-    window.scrollTo(0, 0)
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+    })
 }
