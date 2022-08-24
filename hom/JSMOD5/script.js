f89445635ff0868d735cc6970ddcc8454614c069
listok195 <listok195@gmail.com>
2022-08-24 21:20:07 +0300

    fg

--- hom/JSMOD5/script.js
+++ hom/JSMOD5/script.js
@@ -25,17 +25,38 @@ z1sub.onclick = function (e) {
         z1username.setCustomValidity('Введите корректное имя пользователя')
     }
 }
-z2copass.addEventListener('change', function (e) {
-    if (z2copass.value !== z2pass.value) {
-        z2copass.setCustomValidity('Пароль должен совпадать')
+z2v4.addEventListener('change', function (e) {
+    if (z2v4.value == '') {
+        z2v4.setCustomValidity('Заполните это поле')
+    }
+    else if (z2v4.value !== z2v3.value) {
+        z2v4.setCustomValidity('Пароль должен совпадать')
     }
 })
 z2sub.onclick = function (e) {
-    mass.style.display = 'block'
+    if (z2v1.checkValidity() && z2v2.checkValidity() && z2v3.checkValidity() && z2v4.checkValidity())
+        mass.style.display = 'block'
     mass.classList.add('mass-in')
-    mass.innerHTML = `На почту
-         отправлено письмо с подтверждением`
+    mass.innerHTML = `На почту ${z2v2.value} отправлено письмо с подтверждением`
 
 
     setTimeout(disbl, 3000)
+}
+z3sub.onclick = function () {
+    let gendsel = document.getElementsByClassName('gender')
+    if (!gendsel[0].checked && !gendsel[1].checked) {
+
+        let errmas = document.createElement('div')
+        let after = document.createElement('div')
+        after.id = 'after';
+        errmas.style.position = 'absolute'
+        errmas.style.left = '100px'
+        errmas.style.padding = '3px'
+        errmas.style.bottom = '-30px'
+        errmas.style.borderRadius = '5px'
+        errmas.style.backgroundColor = 'red'
+        errmas.innerHTML = 'Укажите пол'
+        errmas.appendChild(after)
+        gender.appendChild(errmas)
+    }
 }
