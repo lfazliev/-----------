55e16228bffabeb1275442d6fde95cdb6bb8f0db
listok195 <listok195@gmail.com>
2022-09-16 20:59:57 +0300

    f

---
+++ classwork/vue/script.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        massage: 'Hi vue',
+        numb: 0,
+        // methods: {
+        //     add: function () {
+        //         this.numb++
+        //     }
+        // }
+    }
+})
