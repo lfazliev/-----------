7d7e8dd48625f8cac3531a00afb4b9017b009e2f
listok195 <listok195@gmail.com>
2022-09-21 21:32:34 +0300

    g

--- newproj/newsroll/public/script.js
+++ newproj/newsroll/public/script.js
@@ -29,21 +29,31 @@ var app = new Vue({
         const data = await fetch('/news')
         const json = await data.json()
         this.news = json.all
+        for (n of this.news) {
+            n.date = this.getPrDate(n.date)
+            n.tag = this.getPrTag(n.tag)
+        }
     },
     methods: {
         addNews: async function () {
+            const date = this.getPrDate(this.date)
             const result = await fetch('/news', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json;charset=utf-8'
                 },
-                body: JSON.stringify({ header: this.header, text: this.text, tag: this.tag.split(), date: this.date })
+                body: JSON.stringify({ header: this.header, text: this.text, tag: this.getTag(), date: this.date })
             })
             const insertRes = await result.json()
-            this.news.push({ header: this.header, text: this.text, tag: this.tag.split(), date: this.date, _id: insertRes.result.insertedId })
+            this.news.push({
+                header: this.header,
+                text: this.text,
+                tag: this.tag,
+                date,
+                _id: insertRes.result.insertedId
+            })
         },
         delNews: async function (_id) {
-
             this.news.splice(this.news.findIndex(n => n._id == _id), 1)
             const result = await fetch('/news', {
                 method: 'DELETE',
@@ -53,6 +63,66 @@ var app = new Vue({
                 body: JSON.stringify({ id: _id })
             })
             const insertRes = await result.json()
+        },
+        getTag: function () {
+            if (this.tag !== '') {
+                let taglist = this.tag.split(' ')
+                for (let i = 0; i < taglist.length; i++) {
+                    if (taglist[i] == '') {
+                        taglist.splice(i, 1)
+                        i--
+                    }
+                }
+                return taglist
+            }
+            else {
+                return ''
+            }
+        },
+        getPrTag: function (tago) {
+            let TagT
+            if (tago.length !== 0) {
+                TagT = tago.join(' #')
+            }
+            else {
+                TagT = 'Теги не указаны'
+            }
+            return TagT
+        },
+        getPrDate: function (dateT) {
+            let date
+            if (dateT !== '') {
+                dateT = new Date(dateT)
+                let actualdate = new Date()
+                if (actualdate.getDate() == dateT.getDate() && actualdate.getMonth() == dateT.getMonth() && actualdate.getFullYear() == dateT.getFullYear()) {
+                    date = 'Сегодня'
+                } else {
+                    let pol = 0
+                    for (let i = 0; i < 6; i++) {
+                        actualdate.setDate(actualdate.getDate() - 1)
+                        if (actualdate.getDate() == dateT.getDate() && actualdate.getMonth() == dateT.getMonth() && actualdate.getFullYear() == dateT.getFullYear()) {
+                            if (i == 0) {
+                                date = `${i + 1} день назад`
+                            }
+                            else {
+                                if (i > 0 && i < 4) {
+                                    date = `${i + 1} дня назад`
+                                }
+                                else
+                                    date = `${i + 1} дней назад`
+                            }
+                            pol = 1
+                        }
+                    }
+                    if (pol == 0) {
+                        date = dateT.toLocaleDateString()
+                    }
+                }
+            }
+            else {
+                date = 'Дата не указана'
+            }
+            return date
         }
     },
     computed: {
@@ -63,6 +133,7 @@ var app = new Vue({
             else {
                 return this.news
             }
-        }
+        },
+
     }
 })
