86d08a34dc577df4f6d6cf70a97a6f6f6412f5a6
listok195 <listok195@gmail.com>
2022-10-21 21:34:18 +0300

    dd

---
+++ blogVue/client/vue.config.js
@@ -0,0 +1,9 @@
+// vue.config.js
+
+/**
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
+// eslint-disable-next-line no-undef
+export const publicPath = process.env.NODE_ENV === 'production'
+    ? '/blogVue/client/'
+    : '/';
