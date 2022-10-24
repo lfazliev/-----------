3bdd34780519083bd2ad3c523e5fd97ed410d549
Lenar <listok195@gmail.com>
2022-10-24 21:28:09 +0300

    f

--- newproj/src/stores/items.js
+++ newproj/src/stores/items.js
@@ -13,7 +13,11 @@ export const useItemsStore = defineStore('items', {
             const length = Math.random() * (10 - 4) + 4
             for (let i = 0; i <= length; i++) {
                 const count = Math.random() * (10 - 4) + 4
-                const color = 0 // заполнить рандомным цветом
+                let letters = '0123456789ABCDEF';
+                let color = '#'
+                for (let i = 0; i < 6; i++) {
+                    color += letters[Math.floor(Math.random() * 16)];
+                }
                 state.items.push({ count, color })
             }
         }
